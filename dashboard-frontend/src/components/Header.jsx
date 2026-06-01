792ae6dcd4e5d3c7029bad4f30dc8ed2e3288c6c
Siti-Salva-Eka-Gita <mylife6726@gmail.com>
2026-06-01 20:45:23 +0700

    Update Header.jsx

--- dashboard-frontend/src/components/Header.jsx
+++ dashboard-frontend/src/components/Header.jsx
@@ -12,19 +12,6 @@ export default function Header({ darkMode, setDarkMode, setActivePage }) {
         </h1>
       </div>
 
-      {/* search */}
-      <div className="absolute left-1/2 -translate-x-1/2 max-w-md w-full hidden md:flex items-center gap-2 bg-white dark:bg-Black-Light border border-gray-200 dark:border-GreyBorder rounded-xl px-3 py-2">
-        <IoSearch
-          size={16}
-          className="text-gray-400 shrink-0"
-        />
-        <input
-          type="text"
-          placeholder="Cari produk, transaksi, dll..."
-          className="bg-transparent text-sm text-gray-600 dark:text-gray-300 placeholder-gray-400 outline-none flex-1"
-        />
-      </div>
-
       {/* right section */}
       <div className="flex items-center gap-2">
         {/* dark mode button */}
@@ -44,8 +31,8 @@ export default function Header({ darkMode, setDarkMode, setActivePage }) {
           onClick={() => setActivePage('pengaturan')}
           className="flex items-center justify-center gap-2 cursor-pointer dark:hover:bg-white/10 rounded-xl w-35 h-12 transition-all duration-300 ease-in-out">
           {/* avatar */}
-          <div className="w-8 h-8 rounded-full bg-Biru  flex items-center justify-center text-white text-xs font-bold">
-            <FaUser size={15} />
+          <div className="w-8 h-8 rounded-full bg-Biru flex items-center justify-center overflow-hidden">
+            <img src="/logo.svg" alt="Luxora" className="w-5 h-5 object-contain" />
           </div>
           {/* admin info */}
           <div className="hidden sm:block">
